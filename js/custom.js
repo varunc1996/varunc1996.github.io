e05ef7f15b3abacbf9ac2c16215b9040a3e34518
varunc1996 <varunc1996@gmail.com>
2015-06-24 15:08:02 -0700

    dfs

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
 	new WOW().init();
 
 	jQuery(window).load(function() { 
-		jQuery("#preloader").delay(100)fadeOut("slow");
+		jQuery("#preloader").delay(100).fadeOut("slow");
 		jQuery("#load").delay(100).fadeOut("slow");
 	});
 
