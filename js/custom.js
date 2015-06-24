2989c48ff9aaf7467796badb88afe7afdc945657
varunc1996 <varunc1996@gmail.com>
2015-06-24 15:05:09 -0700

    fad

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,8 @@
 	new WOW().init();
 
 	jQuery(window).load(function() { 
-		jQuery("#preloader").delay(50)fadeOut("slow");
-		jQuery("#load").delay(50).fadeOut("slow");
+		jQuery("#preloader").delay(100)fadeOut("slow");
+		jQuery("#load").delay(100).fadeOut("slow");
 	});
 
 
