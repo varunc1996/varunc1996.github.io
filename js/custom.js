7184b02b6c198c6ecb34e5505a17300a25f40dc0
varunc1996 <varunc1996@gmail.com>
2015-06-24 15:00:03 -0700

    uil

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@
 
 	//jQuery to collapse the navbar on scroll
 	$(window).scroll(function() {
-		if ($(".navbar").offset().top > 200) {
+		if ($(".navbar").offset().top > 300) {
 			$(".navbar-fixed-top").addClass("top-nav-collapse");
 		} else {
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
