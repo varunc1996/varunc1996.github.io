8a5aa3ae7607f0d7b38fa4b52b63cfbe98fd9da7
varunc1996 <varunc1996@gmail.com>
2015-06-24 15:02:23 -0700

    i

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,8 @@
 	new WOW().init();
 
 	jQuery(window).load(function() { 
-		jQuery("#preloader").delay(100).fadeOut("slow");
-		jQuery("#load").delay(100).fadeOut("slow");
+		jQuery("#preloader").fadeOut("slow");
+		jQuery("#load").fadeOut("slow");
 	});
 
 
