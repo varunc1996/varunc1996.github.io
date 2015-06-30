64b08a9c5c9471ecd8a1326a361497ad7f42c6d4
varunc1996 <varunc1996@gmail.com>
2015-06-30 10:08:53 -0700

    Yellow and brown

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,7 @@
 
 	//jQuery to collapse the navbar on scroll
 	$(window).scroll(function() {
-		if ($(".navbar").offset().top > 750) {
+		if ($(".navbar").offset().top > 300) {
 			$(".navbar-fixed-top").addClass("top-nav-collapse");
 		} else {
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
